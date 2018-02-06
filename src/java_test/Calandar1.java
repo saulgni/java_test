59bdc0accc8808c299dc874770588e2eb20a2be8
axis98 <axis98@naver.com>
2018-02-06 16:09:44 +0900

    update month

--- src/java_test/Calandar1.java
+++ src/java_test/Calandar1.java
@@ -1,5 +1,7 @@
 package java_test;
 
+import java.util.Scanner;
+
 public class Calandar1 {
 	public static void main(String[] args) {
 		System.out.println(" 일  월   화   수  목   금   토");
@@ -9,6 +11,17 @@ public class Calandar1 {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 		System.out.println("29 30 31");
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("달을 입력하세요");
+		int month = scanner.nextInt();
+		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		System.out.printf("%d월은 %d일까지 있습니다. \n", month, maxDays[month - 1]);
+		scanner.close();
+		
+			
 		
 	}
 }
