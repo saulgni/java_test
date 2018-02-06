b5b1944f46b77b75ca548eb72fa5ef268c4cc4fa
axis98 <axis98@naver.com>
2018-02-06 23:17:49 +0900

    Prompt class made

--- src/java_test/Calandar1.java
+++ src/java_test/Calandar1.java
@@ -11,51 +11,28 @@ public class Calandar1 {
 		return MAX_DAYS[month -1];
 	}
 	
-	public void printSampleCalandar() {
-		System.out.println(" 일  월   화   수  목   금   토");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-		System.out.println("29 30 31");
+	public void printCalandar(int year,int month) {
+		System.out.printf("     <<%4d년%3d월>>\n",year,month);
+		System.out.println(" Sun Mon Tue Wed Thu Fri Sat");
+		System.out.println("----------------------------");
 		
-	}
-	
-	
-	public static void main(String[] args) {
-			
-		
-		String PROMPT = "cal> ";
-		Scanner scanner = new Scanner(System.in);
-		Calandar1 cal = new Calandar1();
-	
-
-		
-		int month = 1;
-		
-		while (true) {
-			System.out.println("달을 입력하세요");
-			System.out.print(PROMPT);
-			month = scanner.nextInt();
-			
-			if(month == -1) {
-				break;
+		int maxDay = getMaxDaysOfMonth(month);
+		for (int i = 1; i <= maxDay; i++) {
+			System.out.printf("%4d",i);
+				if ( i % 7 == 0) {
+				System.out.println();
 			}
-			if(month > 12) {
-				continue;
-			}
-			System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDaysOfMonth(month));	
-			
-			
+				
 		}
+		System.out.println();
 		
-		System.out.println("The End !!.");
-		
-	//	cal.printSampleCalandar();
-		scanner.close();
-		
-			
+//		System.out.println(" 1  2  3  4  5  6  7");
+//		System.out.println(" 8  9 10 11 12 13 14");
+//		System.out.println("15 16 17 18 19 20 21");
+//		System.out.println("22 23 24 25 26 27 28");
+//		System.out.println("29 30 31");
 		
 	}
-}
+	
+	
+}	
